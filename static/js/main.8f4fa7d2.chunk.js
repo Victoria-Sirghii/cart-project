236eb5feee68c951285a40dee9764c9810664130
Victoria <vika.sirghii@gmail.com>
2021-04-14 19:25:52 +0200

    Updates

---
+++ static/js/main.8f4fa7d2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcart-project"]=this["webpackJsonpcart-project"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);c(1);var r=c(3),n=c.n(r),s=(c(8),c(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("header",{className:"App-header",children:"hello"})})};n.a.render(Object(s.jsx)(a,{}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.8f4fa7d2.chunk.js.map
