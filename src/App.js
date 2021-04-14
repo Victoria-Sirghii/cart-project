d23f1687a3d79bd9185dcf142fb5d05b0079f7fe
Victoria <vika.sirghii@gmail.com>
2021-04-14 19:34:23 +0200

    created all files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+// items
 
 function App() {
+  // if (loading) {
+  //   return (
+  //     <div className='loading'>
+  //       <h1>Loading...</h1>
+  //     </div>
+  //   )
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-      hello
-      </header>
-    </div>
-  );
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
+import { AppProvider } from './context';
 ReactDOM.render(
-    <App />,
+    <AppProvider>
+      <App />
+    </AppProvider>,
   document.getElementById('root')
 );
-
-
